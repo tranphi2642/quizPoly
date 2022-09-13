d53ef1557cdf579a1e91789a3af4e46344241798
Trần Mậu Phi <84634264+tranphi2642@users.noreply.github.com>
2022-09-13 21:37:39 +0700

    update

--- src/views/Contact/index.jsx
+++ src/views/Contact/index.jsx
@@ -1,5 +1,110 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./index.scss";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section className="mt-2">
+      <div className="section-header" style={{ paddingLeft: 100 }}>
+        <div className="chat-bubble">
+          <span className="title">#TAG LIÊN HỆ</span>
+          <div
+            className="chat-bubble reverse"
+            style={{ color: "#27b995" }}
+          ></div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="form">
+          <div className="contact-info">
+            <h3 className="title">Hãy giữ liên lạc</h3>
+            <p className="text">
+              Liên hệ với chúng tôi với các chi tiết sau đây và điền vào biểu
+              mẫu với các chi tiết.
+            </p>
+            <div className="info">
+              <div className="social-information">
+                <i className="fa fa-map-marker"></i>
+                <p>TP.HCM</p>
+              </div>
+              <div className="social-information">
+                <i className="fa fa-envelope-o"></i>
+                <p>phitmps16509@fpt.edu.vn</p>
+              </div>
+              <div className="social-information">
+                <i className="fa fa-mobile-phone"></i>
+                <p>0377166208</p>
+              </div>
+            </div>
+            <div className="social-media">
+              <p>Liên hệ với chúng tôi</p>
+              <div className="social-icons">
+                <Link to="/">
+                  <i className="fa fa-facebook-f"></i>
+                </Link>
+                <Link to="/">
+                  <i className="fa fa-twitter"></i>
+                </Link>
+                <Link to="/">
+                  <i className="fa fa-instagram"></i>
+                </Link>
+                <Link to="/">
+                  <i className="fa fa-linkedin"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="contact-info-form">
+            <span className="circle one"></span>{" "}
+            <span className="circle two"></span>
+            <form action="#" autoComplete="off">
+              <h3 className="title">Liên hệ </h3>
+              <div className="social-input-containers1">
+                <input
+                  type="text"
+                  name="name"
+                  className="input"
+                  placeholder="Tên tài khoản"
+                />
+              </div>
+              <div className="social-input-containers">
+                <input
+                  type="email"
+                  name="email"
+                  className="input"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="social-input-containers">
+                <input
+                  type="tel"
+                  name="phone"
+                  className="input"
+                  placeholder="Số điện thoại"
+                />
+              </div>
+              <div className="social-input-containers textarea">
+                <textarea
+                  name="message"
+                  className="input"
+                  placeholder="Lời nhắn"
+                ></textarea>
+              </div>
+              <input type="submit" value="Gửi" className="btn1" />
+            </form>
+          </div>
+        </div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.304446915596!2d106.61291731471928!3d10.864432792261871!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752a172d8c987f%3A0x0!2zMTDCsDUxJzUyLjAiTiAxMDbCsDM2JzU0LjQiRQ!5e0!3m2!1svi!2s!4v1647188028669!5m2!1svi!2s"
+          width="425"
+          height="520"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+        ></iframe>
+      </div>
+    </section>
+  );
 }
--- src/views/Register/index.jsx
+++ src/views/Register/index.jsx
@@ -1,5 +1,133 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/images/banner/img_avatar2.png";
+import { Link } from "react-router-dom";
+
+import "./index.scss";
 
 export default function Register() {
-  return <div>Register</div>
+  return (
+    <div className="signup">
+      <div className="logo">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="text-center mt-4 name">Đăng ký tài khoản </div>
+      <div className="row">
+        <form className="p-3 mt-2" name="signup">
+          <div className="row p-2">
+            <div className="form-field d-flex align-items-center col-sm-5 ">
+              <span className="fa fa-user"></span>
+              <input
+                type="text"
+                name="username"
+                id="userName"
+                placeholder="Tài khoản"
+              />
+            </div>
+
+            <div className="col-sm-2"></div>
+
+            <div className="form-field d-flex align-items-center col-sm-5 ">
+              <span className="fa fa-key"></span>
+              <input
+                type="password"
+                name="password"
+                id="pwd"
+                placeholder="Mật khẩu"
+              />
+            </div>
+          </div>
+
+          <div className="form-field d-flex align-items-center">
+            <span className="fa fa-user-circle"></span>
+            <input
+              type="text"
+              name="fullname"
+              id="hoten"
+              placeholder="Họ tên"
+            />
+          </div>
+
+          <div className="form-field d-flex align-items-center">
+            <span className="fa fa-at"></span>
+            <input type="email" name="email" id="email" placeholder="Email" />
+          </div>
+
+          <div className="row p-3">
+            <div className="form-check col-sm-6">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                value="Nam"
+                id="flexRadioDefault1"
+                defaultChecked={true}
+              />
+              <label className="form-check-label" htmlFor="flexRadioDefault1">
+                <span className="fa fa-mars" style={{ fontSize: 20 }}>
+                  Nam
+                </span>
+              </label>
+            </div>
+
+            <div className="form-check col-sm-6">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                value="Nữ"
+                id="flexRadioDefault2"
+              />
+              <label className="form-check-label" htmlFor="flexRadioDefault2">
+                <span className="fa fa-venus" style={{ fontSize: 20 }}>
+                  Nữ
+                </span>
+              </label>
+            </div>
+          </div>
+
+          <div className="form-field d-flex align-items-center">
+            <span className="fa fa-calendar"></span>
+            <input
+              type="text"
+              name="birthday"
+              id="date"
+              placeholder="Ngày sinh"
+            />
+          </div>
+
+          <div className="row p-2">
+            <div className="form-field d-flex align-items-center col-sm-5">
+              <span className="fa fa-money"></span>
+              <input
+                type="number"
+                name="schoolfree"
+                min="0"
+                id="schoolfree"
+                placeholder="Học phí"
+              />
+            </div>
+
+            <div className="col-sm-2"></div>
+
+            <div className="form-field d-flex align-items-center col-sm-5">
+              <span className="fa fa-hand-pointer-o"></span>
+              <input
+                type="number"
+                name="marks"
+                min="0"
+                max="10"
+                id="marks"
+                placeholder="Điểm"
+              />
+            </div>
+          </div>
+          <button className="btn mt-3">Đăng ký</button>
+        </form>
+        <div className="text-center fs-6">
+          <Link to="/">Quên mật khẩu?</Link> hoặc
+          <Link to="/login"> Đăng nhập</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
